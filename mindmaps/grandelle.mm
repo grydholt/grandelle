cde81eba0217d417d6a594ac7a9e1ce59242e16c
Jacob Grydholt Jensen <grydholt@gmail.com>
2009-05-24 00:11:45 +0200

    Added empty mindmap

---
+++ mindmaps/grandelle.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1243115672328" ID="ID_399096354" MODIFIED="1243115765000" TEXT="grandelle"/>
+</map>
