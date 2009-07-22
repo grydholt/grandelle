9b9b4e9465122ece5f51ca79f94387782f656065
Jacob Grydholt Jensen <grydholt@gmail.com>
2009-07-22 23:28:26 +0200

    First real version

--- mindmaps/grandelle.mm
+++ mindmaps/grandelle.mm
@@ -1,4 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1243115672328" ID="ID_399096354" MODIFIED="1243115765000" TEXT="grandelle"/>
+<node CREATED="1243115672328" ID="ID_399096354" MODIFIED="1243115765000" TEXT="grandelle">
+<node CREATED="1248298008828" ID="ID_1058958988" MODIFIED="1248298017437" POSITION="right" TEXT="I&apos;m going to run the lift examples"/>
+</node>
 </map>
