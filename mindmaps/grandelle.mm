249390e80949a60d805e9d411db352058f58cfb1
Jacob Grydholt Jensen <grydholt@gmail.com>
2009-07-23 22:42:38 +0200

    Checked stuff in on the maven archetypes

--- mindmaps/grandelle.mm
+++ mindmaps/grandelle.mm
@@ -1,6 +1,26 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1243115672328" ID="ID_399096354" MODIFIED="1243115765000" TEXT="grandelle">
-<node CREATED="1248298008828" ID="ID_1058958988" MODIFIED="1248298017437" POSITION="right" TEXT="I&apos;m going to run the lift examples"/>
+<node CREATED="1243115672328" ID="ID_399096354" MODIFIED="1248381637948" TEXT="grandelle">
+<node CREATED="1248298008828" ID="ID_1058958988" MODIFIED="1248298017437" POSITION="right" TEXT="I&apos;m going to run the lift examples">
+<node CREATED="1248381647970" ID="ID_1683878020" MODIFIED="1248381668513">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      maven archetypes
+    </p>
+    <p>
+      er godt nok h&#229;rde
+    </p>
+    <p>
+      ved kommandolinjen
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </map>
